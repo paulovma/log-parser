70952a666e93a7caa86222693bbf97459fd2ed48
Paulo Amaral <paulosilvav@gmail.com>
2021-03-10 07:26:36 -0300

    Inicializacao da aplicacao

---
+++ src/main/java/com/quake/arena/logparser/LogParserApplication.java
@@ -0,0 +1,15 @@
+package com.quake.arena.logparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@EnableReactiveMongoRepositories
+@SpringBootApplication
+public class LogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogParserApplication.class, args);
+	}
+
+}
